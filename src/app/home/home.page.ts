2e4a0e91c7507a2764cd0e953b0ec3bec0ea6f7d
Gustavo Ferreira Cordeiro <115183614+gucordeiro26@users.noreply.github.com>
2023-06-25 18:24:14 +0000

    Finalizando a calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Type } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,14 +7,38 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  
-  resultado: any = 0;
+  exibirValor = '';
 
-  somar(valor: any) {
-    this.resultado = valor + valor;
-    alert(this.resultado);
+  clickBotao(button: string) {
+    this.exibirValor += button;
+  }
+
+  Limpar() {
+    this.exibirValor = '';
+  }
+
+  Calcular() {
+    try {
+      const resultado = eval(this.exibirValor);
+      this.exibirValor = resultado.toString();
+    } catch (error) {
+      this.exibirValor = 'Error';
+    }
+  }
+
+  calcularPercentual() {
+    try {
+      const percentage = eval(this.exibirValor) / 100;
+      this.exibirValor = percentage.toString();
+    } catch (error) {
+      this.exibirValor = 'Error';
+    }
+  }
+
+  apagarUltimaNumero() {
+    this.exibirValor = this.exibirValor.slice(0, -1);
   }
 }
