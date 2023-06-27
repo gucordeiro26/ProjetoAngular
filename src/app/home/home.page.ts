53584c12bfb92461ac1671221d7a8ec8e16f8cb0
Gustavo Ferreira Cordeiro <115183614+gucordeiro26@users.noreply.github.com>
2023-06-27 01:51:55 +0000

    Resultado final da calculadora funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,36 +9,61 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule, CommonModule],
 })
+
 export class HomePage {
+
+  // variável criada para receber todos os valores dos cliques dos botões do html e armazenar como string
+  // this é usado sempre que preciso pegar a variável de fora da função e usar dentro delas
   exibirValor = '';
 
-  clickBotao(button: string) {
-    this.exibirValor += button;
+  // função criada para armazenar cada valor do clique dos botões
+  clickBotao(botao: string) {
+    if (botao === '%') {
+      this.calcularPercentual();
+    } else {
+      this.exibirValor += botao;
+    }
   }
 
+  // função criada para excluir todos os valores selecionados por clique anteriormente
   Limpar() {
     this.exibirValor = '';
   }
 
+  // função criada para calcular a expressão matemática do usuário
+  // try/catch é usado para verificar erros durante a verificação da expressão matemática criada pelo usuário, sendo try a parte executada se não tiver erro e o catch consegue captar o erro e retorna uma mensagem de erro
+  // função nativa eval() é usada para avaliar se o meu texto é um código válido em JS para ser executado, aqui o exibirValor passa a ser number
+  // variável resultado é usada para receber o valor da função nativa eval()
+  // toString() foi usado para converter o resultado do cálculo da expressão em uma string
   Calcular() {
     try {
       const resultado = eval(this.exibirValor);
       this.exibirValor = resultado.toString();
-    } catch (error) {
-      this.exibirValor = 'Error';
+    } catch (_) {
+      this.exibirValor = 'Algo Deu Errado!';
     }
   }
 
+  // função criada expecificamente para um cálculo, de percentual
+  // também usando o try/catch, ela tem uma variável percentual que igual a variável resultado, recebe o valor final da expressão
+  // a variável porcentagem recebe o resultado do cálculo da expressão matemática
+  // neste cálculo de percentual o eval transforma o texto dos botões em number e faz o cálculo
+  // por fim é acrescentado ao resultado o sinal de multiplicação
   calcularPercentual() {
     try {
-      const percentage = eval(this.exibirValor) / 100;
-      this.exibirValor = percentage.toString();
+      const porcentagem = eval(this.exibirValor) / 100;
+      this.exibirValor = porcentagem.toString();
+      this.exibirValor = this.exibirValor + '*';
     } catch (error) {
-      this.exibirValor = 'Error';
+      this.exibirValor = 'Algo Deu Errado!';
     }
+
   }
 
-  apagarUltimaNumero() {
+  // função criada para apagar somente o último valor digitado pelo usuário
+  // função nativa slice() é usada para extrair uma parte de uma string, especificando o índice inicial o final
+  apagarUltimoNumero() {
     this.exibirValor = this.exibirValor.slice(0, -1);
   }
+
 }
